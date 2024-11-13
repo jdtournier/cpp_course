8bc9042b54f0f1ee8d54904a214e706adcd800c5
J-Donald Tournier <jdtournier@gmail.com>
2024-11-13 17:15:06 +0000

    Next, add code to read the data from file
    We use the C++ input file stream interface to open the file, then
    read each fragment into a string, and append to a vector of strings.
    We also check for any errors when opening the file, and check that the
    file does contain at least 1 fragment.

--- projects/DNA_shotgun_sequencing/solution/shotgun.cpp
+++ projects/DNA_shotgun_sequencing/solution/shotgun.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
 
 int main (int argc, char* argv[])
 {
@@ -7,6 +10,24 @@ int main (int argc, char* argv[])
     return 1;
   }
   std::cerr << "reading fragments from file \"" << argv[1] << "\"...\n";
+
+  std::ifstream infile (argv[1]);
+  if (!infile) {
+    std::cerr << "ERROR: failed to open file \"" << argv[1] << "\" - aborting\n";
+    return 1;
+  }
+
+  std::vector<std::string> fragments;
+  std::string frag;
+  while (infile >> frag)
+    fragments.push_back (frag);
+
+  if (fragments.empty()) {
+    std::cerr << "ERROR: file \"" << argv[1] << "\" contains no fragments - aborting\n";
+    return 1;
+  }
+
+  std::cerr << "read " << fragments.size() << " fragments\n";
 
   return 0;
 }
