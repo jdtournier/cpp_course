7804c4aeeb18ee3dcdd9a5964db5508093e31d4f
J-Donald Tournier <jdtournier@gmail.com>
2024-11-14 19:26:26 +0000

    Split code over multiple header and cpp files
    In practice, it is best to split up the code into smaller, more
    manageable chunks. This works best when distinct aspects of the
    functionality can be grouped into logical 'units' or modules, which can
    be inspected and understood independently.
    
    To do this, we need to place the *declarations* of our various functions
    in the *header* file, so that other parts of the code have the
    information required to use them (name, number and type of arguments,
    return type, etc). We then place the *definitions* in a separate cpp
    file (typically with the same name), which can be *compiled* separately,
    and the resulting object file can then be *linked* into the final
    executable at a later stage.
    
    This code can be compiled and linked using the following commands:
    
    g++ -std=c++20 -c shotgun.cpp -o shotgun.o
    g++ -std=c++20 -c fragments.cpp -o fragments.o
    g++ shotgun.o fragments.o -o shotgun

--- projects/DNA_shotgun_sequencing/solution/shotgun.cpp
+++ projects/DNA_shotgun_sequencing/solution/shotgun.cpp
@@ -1,65 +1,8 @@
 #include <iostream>
-#include <vector>
 #include <string>
-#include <fstream>
+#include <stdexcept>
 
-
-std::vector<std::string> load_fragments (const std::string& filename)
-{
-  std::cerr << "reading fragments from file \"" << filename << "\"...\n";
-
-  std::ifstream infile (filename);
-  if (!infile)
-    throw std::runtime_error ("failed to open file \"" + filename + "\"");
-
-  std::vector<std::string> fragments;
-  std::string frag;
-  while (infile >> frag)
-    fragments.push_back (frag);
-
-  if (fragments.empty())
-    throw std::runtime_error ("file \"" + filename + "\" contains no fragments");
-
-  std::cerr << "read " << fragments.size() << " fragments\n";
-
-  return fragments;
-}
-
-
-
-
-void fragment_statistics (const std::vector<std::string>& fragments)
-{
-  double sum = 0.0;
-  auto min = fragments[0].size();
-  auto max = fragments[0].size();
-  for (const auto& f : fragments) {
-    sum += f.size();
-    min = std::min (min, f.size());
-    max = std::max (max, f.size());
-  }
-  std::cerr << "mean fragment length: " << sum/fragments.size() << ", range [ " << min << " " << max << " ]\n";
-}
-
-
-
-
-void write_sequence (const std::string& filename, const std::string& sequence)
-{
-  std::cerr << "writing sequence to file \"" << filename << "\"...\n";
-  std::ofstream outfile (filename);
-  if (!outfile)
-    throw std::runtime_error ("failed to open output file \"" + filename + "\"");
-
-  outfile << sequence << "\n";
-
-  if (!outfile)
-    throw std::runtime_error ("error occurred while writing to output file \"" + filename + "\"");
-}
-
-
-
-
+#include "fragments.h"
 
 int main (int argc, char* argv[])
 {
