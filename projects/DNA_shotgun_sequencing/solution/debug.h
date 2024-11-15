c3013cf161d91590859f24b76e63794afec7d33a
J-Donald Tournier <jdtournier@gmail.com>
2024-11-15 20:41:48 +0000

    Add -v command-line option for more verbose output
    Most practical commands accept *options* in addition to their mandatory
    arguments. Many commands accept an option to enable some form of
    *verbose mode*, which provides more detailed feedback information about
    the process, and is very useful when debugging.
    
    Here, we look for the "-v" option in the argument list, and if found, we
    enable verbose mode and remove the option from the argument list.  We
    also store the information about whether verbose mode is enabled in a
    *global variable*. Global variables are almost always a bad idea, but
    are acceptable when used to represent an entity that is guaranteed to be
    unique (std::cout / std::cerr are good examples of this). However, to
    avoid naming conflicts and reduce the scope for name shadowing, it is
    best to declare this variable within its own bespoke *namespace*.

---
+++ projects/DNA_shotgun_sequencing/solution/debug.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace debug {
+
+  inline bool verbose = false;
+
+  void set_verbose_mode (int& argc, char* argv[]);
+
+}
--- projects/DNA_shotgun_sequencing/solution/shotgun.cpp
+++ projects/DNA_shotgun_sequencing/solution/shotgun.cpp
@@ -4,9 +4,12 @@
 
 #include "fragments.h"
 #include "overlap.h"
+#include "debug.h"
 
 int main (int argc, char* argv[])
 {
+  debug::set_verbose_mode (argc, argv);
+
   if (argc < 3) {
     std::cerr << "ERROR: missing arguments - expected 2 arguments: input_fragments output_sequence\n";
     return 1;
@@ -15,14 +18,17 @@ int main (int argc, char* argv[])
   try { // Start of main processing:
 
     auto fragments = load_fragments (argv[1]);
-    fragment_statistics (fragments);
+    if (debug::verbose)
+      fragment_statistics (fragments);
 
     auto sequence = extract_longest_fragment (fragments);
     std::cerr << "initial sequence has size " << sequence.size() << "\n";
 
     while (fragments.size()) {
-      std::cerr << "---------------------------------------------------\n";
-      std::cerr << fragments.size() << " fragments left\n";
+      if (debug::verbose) {
+        std::cerr << "---------------------------------------------------\n";
+        std::cerr << fragments.size() << " fragments left\n";
+      }
       int biggest_overlap = 0;
       int fragment_with_biggest_overlap = -1;
       for (int n = 0; n < static_cast<int> (fragments.size()); ++n) {
@@ -38,8 +44,9 @@ int main (int argc, char* argv[])
       if (fragment_with_biggest_overlap < 0 || std::abs (biggest_overlap) < 10)
         break;
 
-      std::cerr << "fragment with biggest overlap is at index " << fragment_with_biggest_overlap
-        << ", overlap = " << biggest_overlap << "\n";
+      if (debug::verbose)
+        std::cerr << "fragment with biggest overlap is at index " << fragment_with_biggest_overlap
+          << ", overlap = " << biggest_overlap << "\n";
 
       merge (sequence, fragments[fragment_with_biggest_overlap], biggest_overlap);
       fragments.erase (fragments.begin() + fragment_with_biggest_overlap);
