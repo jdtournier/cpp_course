d88a13b1e0804c6496b43e1a62546e83b4ea399b
J-Donald Tournier <jdtournier@gmail.com>
2024-11-28 15:21:55 +0000

    Introduce debug::log() convenience function
    Since it's likely we'll want to add lots of debugging statements, it's
    helpful to define a small function to reduce the amount of code
    duplication and improve readability. In this case, we define an inline
    function as this is a short function. Furthermore, using an inline
    function allows the compiler to optimise away the function call, which
    can help with performance.
    
    We also add in the VAR() macro to help with debugging, as it may come in
    handy later to figure why things don't work as expected.

--- projects/DNA_shotgun_sequencing/solution/debug.h
+++ projects/DNA_shotgun_sequencing/solution/debug.h
@@ -1,9 +1,20 @@
 #pragma once
 
+#include <iostream>
+#include <string>
+
+#define VAR(x) std::cerr << __FILE__ << ", in " << __func__ << "(), line " << __LINE__  << ": " << #x << " = " << x << "\n"
+
 namespace debug {
 
   inline bool verbose = false;
 
   void set_verbose_mode (int& argc, char* argv[]);
+
+  inline void log (const std::string& message)
+  {
+    if (verbose)
+      std::cerr << "[DEBUG] " << message << "\n";
+  }
 
 }
