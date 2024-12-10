7be6f2bb7ae0a34398d491314f885bb149b55876
J-Donald Tournier <jdtournier@gmail.com>
2024-12-10 12:04:13 +0000

    Initial skeleton for project
    As before, we start from the same basic structure as used in the
    previous projects.

---
+++ projects/robot_arm/solution/robot.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <stdexcept>
+
+#include "debug.h"
+
+int main (int argc, char* argv[])
+{
+  debug::set_verbose_mode (argc, argv);
+
+  try { // Start of main processing:
+
+
+  } // end of main processing
+
+  // error handling from here:
+  catch (std::exception& excp) {
+    std::cerr << "ERROR: " << excp.what() << " - aborting\n";
+    return 1;
+  }
+  catch (...) {
+    std::cerr << "ERROR: unknown exception thrown - aborting\n";
+    return 1;
+  }
+
+  return 0;
+}
