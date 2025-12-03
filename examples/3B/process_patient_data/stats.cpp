79677f47424cd0ddf3e8018e24282f721cc528cc
J-Donald Tournier <jdtournier@gmail.com>
2025-12-03 17:41:40 +0000

    Add function to compute standard deviation

--- examples/3B/process_patient_data/stats.cpp
+++ examples/3B/process_patient_data/stats.cpp
@@ -1,6 +1,13 @@
 #include <vector>
+#include <cmath>
 
 #include "stats.h"
+
+float pow2 (float x)
+{
+  return x*x;
+}
+
 
 float compute_mean (const std::vector<float>& data)
 {
@@ -8,4 +15,15 @@ float compute_mean (const std::vector<float>& data)
   for (const auto& value : data)
     sum += value;
   return sum / data.size();
+}
+
+
+
+float compute_stddev (const std::vector<float>& data)
+{
+  const float mean = compute_mean (data);
+  float sum_diff = 0.0;
+  for (const auto& value : data)
+    sum_diff += pow2 (value-mean);
+  return std::sqrt(sum_diff / (data.size()-1));
 }
