c94c08e5c7fdeee7968d8c005f25fab9b677cd37
J-Donald Tournier <jdtournier@gmail.com>
2025-12-03 17:44:37 +0000

    Declare the pow2() function inline

--- examples/3B/process_patient_data/stats.h
+++ examples/3B/process_patient_data/stats.h
@@ -2,6 +2,7 @@
 
 #include <vector>
 
-float pow2 (float x);
+inline float pow2 (float x) { return x*x; }
+
 float compute_mean (const std::vector<float>& data);
 float compute_stddev (const std::vector<float>& data);
