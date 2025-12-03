031a6a68ddab23c4c584a98bd89b187739b9770e
J-Donald Tournier <jdtournier@gmail.com>
2025-12-03 18:07:30 +0000

    Add convenience logging functions to help with debugging

--- examples/3B/process_patient_data/debug.h
+++ examples/3B/process_patient_data/debug.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <iostream>
+#include <string>
+
 // We define a global variable to specify whether verbose mode is enabled.
 // By default, this should be false.
 inline bool verbose_mode = false;
@@ -7,3 +10,9 @@ inline bool verbose_mode = false;
 // check for presence of option_name in command-line arguments.
 // If found, remove the option from the argument list, and set the verbose_mode global variable to true.
 void set_verbose_mode (int& argc, char* argv[]);
+
+inline void log (const std::string& text)
+{
+  if (verbose_mode)
+    std::cerr << "[DEBUG] " << text << "\n";
+}
