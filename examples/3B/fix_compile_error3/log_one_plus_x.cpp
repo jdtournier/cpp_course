88898b433d3d506dc6e90dfa8d580f68e7a94b1b
J-Donald Tournier <jdtournier@gmail.com>
2025-12-04 12:43:59 +0000

    Program to compute log(1+x) - with fix

--- examples/3B/fix_compile_error3/log_one_plus_x.cpp
+++ examples/3B/fix_compile_error3/log_one_plus_x.cpp
@@ -2,7 +2,7 @@
 
 #include "log_one_plus_x.h"
 
-double log_one_plus_x (const double x, const int nelem = 10)
+double log_one_plus_x (const double x, const int nelem)
 {
   double value = 0.0;
   for (int n = 1; n <= nelem; n++)
