9c82e7879b5d2b508eb46114330366f96f9ed43b
J-Donald Tournier <jdtournier@gmail.com>
2025-12-04 13:38:52 +0000

    Program to reverse and capitalise its arguments - with fix

--- examples/3B/fix_link_error2/string_utils.cpp
+++ examples/3B/fix_link_error2/string_utils.cpp
@@ -3,6 +3,16 @@
 
 #include "string_utils.h"
 
+std::string invert (const std::string& s)
+{
+  std::string s2 (s.size(), ' ');
+  for (int n = 0; n < s.size(); n++)
+    s2[n] = s[s.size()-1-n];
+
+  return s2;
+}
+
+
 std::string capitalise (const std::string& s)
 {
   std::string s2 = s;
--- examples/3B/fix_link_error2/string_utils.h
+++ examples/3B/fix_link_error2/string_utils.h
@@ -3,14 +3,7 @@
 #include <string>
 
 // flip the string back to front:
-std::string invert (const std::string& s)
-{
-  std::string s2 (s.size(), ' ');
-  for (int n = 0; n < s.size(); n++)
-    s2[n] = s[s.size()-1-n];
-
-  return s2;
-}
+std::string invert (const std::string& s);
 
 // convert every letter to upper case:
 std::string capitalise (const std::string& s);
