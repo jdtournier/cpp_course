9bdfc150d0b520e592f2232a6cc689debb747f84
J-Donald Tournier <jdtournier@gmail.com>
2025-12-04 13:17:18 +0000

    compute mean of arguments - with fix

--- examples/3B/fix_link_error1/stats.cpp
+++ examples/3B/fix_link_error1/stats.cpp
@@ -2,7 +2,7 @@
 
 #include "stats.h"
 
-double compute_mean (const std::vector<double>& data)
+double compute_mean (const std::vector<float>& data)
 {
   double sum = 0.0;
   for (const auto x : data)
