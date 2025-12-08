4d0fefd80da50be46949b5f95795bfe16c28cc05
J-Donald Tournier <jdournier@gmail.com>
2025-12-08 15:49:14 +0000

    add initial starting point for 4A

---
+++ process_study/stats.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <cmath>
+
+#include "stats.h"
+
+float compute_mean (const std::vector<float>& data)
+{
+  float sum = 0.0;
+  for (const auto& value : data)
+    sum += value;
+  return sum / data.size();
+}
+
+
+
+float compute_stddev (const std::vector<float>& data)
+{
+  const float mean = compute_mean (data);
+  float sum_diff = 0.0;
+  for (const auto& value : data)
+    sum_diff += pow2 (value-mean);
+  return std::sqrt(sum_diff / (data.size()-1));
+}
