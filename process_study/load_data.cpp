6a9ba5a9b91df4bd161405b6d2d34d87cd35063b
J-Donald Tournier <jdournier@gmail.com>
2025-12-08 15:49:14 +0000

    add initial starting point for 4A

---
+++ process_study/load_data.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <stdexcept>
+
+#include "debug.h"
+#include "load_data.h"
+
+std::vector<std::vector<float>> load_patient_data (const std::string& filename)
+{
+  debug::log ("loading file \"" + filename + "\"...");
+
+  std::ifstream infile (filename);
+  if (!infile)
+    throw std::runtime_error ("failed to open file \"" + filename + "\" for reading");
+
+  int n = 0;
+  std::string line;
+  std::vector<std::vector<float>> data;
+  while (std::getline (infile, line)) {
+    std::istringstream line_stream (line);
+
+    std::string patient_ID;
+    line_stream >> patient_ID;
+
+    // ignore blank lines:
+    if (!line_stream)
+      continue;
+
+    std::vector<float> pat_data;
+    float value;
+    while (line_stream >> value)
+      pat_data.push_back (value);
+
+    if (pat_data.empty())
+      throw std::runtime_error ("no data for patient \"" + patient_ID + "\"");
+
+    data.push_back (pat_data);
+  }
+
+  if (data.empty())
+    throw std::runtime_error ("file \"" + filename + "\" contains no usable data");
+
+  debug::log ("found " + std::to_string(data.size()) + " patients in file \"" + filename + "\"");
+
+  return data;
+}
---
+++ process_study/load_data.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+std::vector<std::vector<float>> load_patient_data (const std::string& filename);
