41f3f4ed0bfa6e2e3d8a958a5f244189066a50ec
J-Donald Tournier <jdournier@gmail.com>
2025-12-13 16:45:07 +0000

    4A: changes to add Student::add_module_marks() method

---
+++ examples/4A/student_class/student.h
@@ -0,0 +1,34 @@
+#include <array>
+#include <string>
+#include <vector>
+
+struct ModuleMark {
+  const std::string description;
+  float mark, weighting;
+};
+
+
+class Student {
+  public:
+    int get_ID () const { return m_ID; }
+    const std::string& get_name () const { return m_name; }
+    float get_GPA () const { return m_GPA; }
+    const std::array<int,3>& get_DOB () const { return m_dob; }
+
+    void init (const std::string& name, const std::array<int,3>& dob, int ID) {
+      m_name = name;
+      m_ID = ID;
+      m_dob = dob;
+      m_GPA = 0.0;
+    }
+
+    void add_module_mark (const std::string& module_description, float mark, float weighting);
+
+  private:
+    int m_ID = 0;
+    std::string m_name;
+    float m_GPA = 0.0;
+    std::array<int,3> m_dob = { 0, 0, 0 };
+    std::vector<ModuleMark> m_marks;
+};
+
