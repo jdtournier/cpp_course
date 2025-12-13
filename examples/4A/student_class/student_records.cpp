3f7f6ea14458bbff8fc6061c8486fae9c490eb91
J-Donald Tournier <jdournier@gmail.com>
2025-12-13 16:45:07 +0000

    4A: changes to add Student::add_module_marks() method

--- examples/4A/student_class/student_records.cpp
+++ examples/4A/student_class/student_records.cpp
@@ -1,35 +1,17 @@
-#include <array>
-#include <string>
 #include <iostream>
 
-class Student {
-  public:
-    int get_ID () const { return m_ID; }
-    const std::string& get_name () const { return m_name; }
-    float get_GPA () const { return m_GPA; }
-    const std::array<int,3>& get_DOB () const { return m_dob; }
-
-    void init (const std::string& name, const std::array<int,3>& dob, int ID, float GPA = 0.0) {
-      m_name = name;
-      m_ID = ID;
-      m_dob = dob;
-      m_GPA = GPA;
-    }
-
-    void set_GPA (float GPA) { m_GPA = GPA; }
-
-  private:
-    int m_ID = 0;
-    std::string m_name;
-    float m_GPA = 0.0;
-    std::array<int,3> m_dob = { 0, 0, 0 };
-};
+#include "student.h"
 
 int main ()
 {
   Student s;
 
-  s.init ("Jane Doe", { 3, 6, 2001 }, 2349423, 3.8);
+  s.init ("Jane Doe", { 3, 6, 2001 }, 2349423);
+
+  s.add_module_mark ("mathematics", 3.2, 1.0);
+  s.add_module_mark ("physics", 3.5, 2.0);
+  s.add_module_mark ("intro to programming", 2.9, 1.0);
+  s.add_module_mark ("signal processing", 3.7, 2.0);
 
   std::cout << "student: \"" << s.get_name() << "\", DOB: "
       << s.get_DOB()[0] << "/" << s.get_DOB()[1] << "/" << s.get_DOB()[2]
