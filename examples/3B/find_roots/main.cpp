246b4e26652f0ee7062b43afd83a59c140e30594
J-Donald Tournier <jdournier@gmail.com>
2025-12-22 09:34:56 +0200

    Update program to check for zero derivative

--- examples/3B/find_roots/main.cpp
+++ examples/3B/find_roots/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 float eval_function (float x)     { return x*x*x - 3.0*x + 3.0; }
 float eval_derivative (float x)   { return 3.0*x*x - 3.0; }
@@ -6,10 +7,13 @@ float eval_derivative (float x)   { return 3.0*x*x - 3.0; }
 int main()
 {
   float dx, x = 0.0;
-  int n = 0;
 
   do {
-    dx = -eval_function (x) / eval_derivative (x);
+    float dfx = eval_derivative (x);
+    if (dfx == 0.0)
+      throw std::runtime_error ("derivative at x = " + std::to_string(x) + " is zero. "
+                                "Maybe try a different initial guess?");
+    dx = -eval_function (x) / dfx;
     x += dx;
   } while (dx != 0.0);
 
