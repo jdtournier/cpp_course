297b88d8e44ad7b4a880373ebf2fb7f2d29a16bd
J-Donald Tournier <jdournier@gmail.com>
2025-12-24 19:24:42 +0200

    4A: add getters to student class

--- examples/4A/student_class/student_records.cpp
+++ examples/4A/student_class/student_records.cpp
@@ -1,7 +1,14 @@
 #include <array>
 #include <string>
+#include <iostream>
 
 class Student {
+  public:
+    int get_ID () const { return m_ID; }
+    const std::string& get_name () const { return m_name; }
+    float get_GPA () const { return m_GPA; }
+    const std::array<int,3>& get_DOB () const { return m_dob; }
+  private:
     int m_ID = 0;
     std::string m_name;
     float m_GPA = 0.0;
@@ -11,6 +18,9 @@ class Student {
 int main ()
 {
   Student s;
+  std::cout << "student: \"" << s.get_name() << "\", DOB: "
+      << s.get_DOB()[0] << "/" << s.get_DOB()[1] << "/" << s.get_DOB()[2]
+      << ", ID: " << s.get_ID() << ", GPA = " << s.get_GPA() << "\n";
 
   return 0;
 }
