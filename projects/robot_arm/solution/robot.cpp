KCL-BMEIS/OOP

Load parameters from file for full robot arm configuration
We can now add functionality to load the parameters for robot arm from
the files provided. To do this correctly, we introduce the stringstream
class, which allows us to read a full line from the file, and then read
from that line as if it was an independent stream. This allows us to
get the full set of parameters from each line without knowing how many
parameters should be present on each line, and then check that all
lines contain the same number of parameters.

Finally, we can assign the parameters we loaded into the robot arm, and
report the tip position for each time point.

projects/robot_arm/solution/robot.cpp
@@ -7,6 +7,7 @@
 #include <thread>
 
 #include "debug.h"
+#include "load_parameters.h"
 #include "segment/tip.h"
 #include "segment/straight.h"
 #include "segment/bend.h"
@@ -19,6 +20,9 @@
 void run (std::vector<std::string>& args)
 {
   debug::verbose = std::erase (args, "-v");
+
+  if (args.size() < 2)
+    throw std::runtime_error ("expected parameter file as first argument");
 
   // set up robot arm:
 
@@ -34,11 +38,22 @@ void run (std::vector<std::string>& args)
   Segment::Rotate rotate1 (straight1, 5.0);
   Segment::Root root (rotate1);
 
-  for (int n = 0; n <= 20; ++n) {
-    bend1.set_angle (std::numbers::pi * n / 40.0);
-    rotate1.set_angle (std::numbers::pi * n / 40.0);
-    bend2.set_angle (std::numbers::pi * n / 80.0);
-    std::cout << "tip position: " << root.tip_position() << "\n";
+  // read parameter list:
+  auto params = load_parameters (args[1]);
+
+  std::vector<Point> positions;
+
+  for (const auto& p : params) {
+
+    rotate1.set_angle (p[0]);
+    bend1.set_angle (p[1]);
+    rotate2.set_angle (p[2]);
+    bend2.set_angle (p[3]);
+    rotate3.set_angle (p[4]);
+    bend3.set_angle (p[5]);
+
+    positions.push_back (root.tip_position());
+    std::cout << positions.back() << "\n";
   }
 
 }

jdtournier/cpp_course

projects/robot_arm/solution/robot.cpp
@@ -2,6 +2,7 @@
 #include <stdexcept>
 
 #include "debug.h"
+#include "load_parameters.h"
 #include "segment/tip.h"
 #include "segment/straight.h"
 #include "segment/bend.h"
@@ -11,6 +12,11 @@
 int main (int argc, char* argv[])
 {
   debug::set_verbose_mode (argc, argv);
+
+  if (argc < 2) {
+    std::cerr << "expected parameter file as first argument\n";
+    return 1;
+  }
 
   try { // Start of main processing:
 
@@ -28,11 +34,22 @@ int main (int argc, char* argv[])
     Segment::Rotate rotate1 (straight1, 5.0);
     Segment::Root root (rotate1);
 
-    for (int n = 0; n <= 20; ++n) {
-      bend1.set_angle (std::numbers::pi * n / 40.0);
-      rotate1.set_angle (std::numbers::pi * n / 40.0);
-      bend2.set_angle (std::numbers::pi * n / 80.0);
-      std::cout << "tip position: " << root.tip_position() << "\n";
+    // read parameter list:
+    auto params = load_parameters (argv[1]);
+
+    std::vector<Point> positions;
+
+    for (const auto& p : params) {
+
+      rotate1.set_angle (p[0]);
+      bend1.set_angle (p[1]);
+      rotate2.set_angle (p[2]);
+      bend2.set_angle (p[3]);
+      rotate3.set_angle (p[4]);
+      bend3.set_angle (p[5]);
+
+      positions.push_back (root.tip_position());
+      std::cout << positions.back() << "\n";
     }
 
   } // end of main processing