KCL-BMEIS/OOP

Introduce template programming for Image class
This allows us to define a class template, capable of representing an
image where each pixel value is represented using any type of our
choosing. Here, we still use an Image of int in the program, but our
Image class is now capable of representing data of other types if we
need to.

This is particularly useful to store the image of correlation
coefficients, which are best stored as floating-point values in the
range [ -1, 1 ].

projects/fMRI/solution/fmri.cpp
@@ -44,7 +44,7 @@ void run (std::vector<std::string>& args)
   if (x < 0 || y < 0) {
     // if no pixel is specified, compute map of correlation coefficient:
     auto im_corr = correlation_coefficient (task, data);
-    TG::imshow (TG::magnify (im_corr, 4), -1000, 1000, TG::jet());
+    TG::imshow (TG::magnify (im_corr, 4), -1.0, 1.0, TG::jet());
 
     return;
   }

jdtournier/cpp_course

projects/fMRI/solution/fmri.cpp
@@ -76,7 +76,7 @@ int main (int argc, char* argv[])
     // default values if x & y not set (<0):
     if (pixel.x < 0 || pixel.y < 0) {
       auto im_corr = correlation_coefficient (task, data);
-      termviz::imshow (termviz::magnify (im_corr, 4), -1000, 1000, termviz::jet());
+      termviz::imshow (termviz::magnify (im_corr, 4), -1.0, 1.0, termviz::jet());
 
       return 0;
     }