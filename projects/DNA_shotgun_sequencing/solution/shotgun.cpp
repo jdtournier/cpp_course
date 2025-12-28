KCL-BMEIS/OOP

Improve functionality of ShotgunSequencer with convenience methods
We add methods to construct the ShotgunSequencer directly from the
filename, by overloading the constructor. We also add methods to load
and save the results from/to file directly. This helps to further
simplify the invoking code, improving its readability.

projects/DNA_shotgun_sequencing/solution/shotgun.cpp
@@ -16,10 +16,7 @@ void run (std::vector<std::string>& args)
   if (args.size() < 3)
     throw std::runtime_error ("missing arguments - expected 2 arguments: input_fragments output_sequence");
 
-  auto fragments = load_fragments (args[1]);
-
-  ShotgunSequencer solver (10);
-  solver.init (fragments);
+  ShotgunSequencer solver (args[1], 10);
 
   std::cerr << "initial sequence has size " << solver.sequence().size() << "\n";
 
@@ -29,7 +26,7 @@ void run (std::vector<std::string>& args)
 
   std::cerr << "final sequence has length " << solver.sequence().size() << "\n";
 
-  write_sequence (args[2], solver.sequence());
+  solver.save (args[2]);
 }
 
 

jdtournier/cpp_course

projects/DNA_shotgun_sequencing/solution/shotgun.cpp
@@ -18,10 +18,7 @@ int main (int argc, char* argv[])
 
   try { // Start of main processing:
 
-    auto fragments = load_fragments (argv[1]);
-
-    ShotgunSequencer solver (10);
-    solver.init (fragments);
+    ShotgunSequencer solver (argv[1], 10);
 
     std::cerr << "initial sequence has size " << solver.sequence().size() << "\n";
 
@@ -31,7 +28,7 @@ int main (int argc, char* argv[])
 
     std::cerr << "final sequence has length " << solver.sequence().size() << "\n";
 
-    write_sequence (argv[2], solver.sequence());
+    solver.save (argv[2]);
 
   } // end of main processing
 