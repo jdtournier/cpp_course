KCL-BMEIS/OOP

Use C++ exceptions to handle errors
This allows errors occurring within functions to be propagated up to the
invoking code, and handled appropriately where possible. This also
ensures that the function is exited normally, with all variables
properly disposed of, and any cleanup performed properly, including
closing file handles or clearing resources such as memory.

projects/DNA_shotgun_sequencing/solution/shotgun.cpp
@@ -8,27 +8,37 @@ int main (int argc, char* argv[])
 {
   std::vector<std::string> args (argv, argv+argc);
 
-  if (args.size() < 3) {
-    std::cerr << "ERROR: missing arguments - expected 2 arguments: input_fragments output_sequence\n";
+  try {
+
+    if (args.size() < 3)
+      throw std::runtime_error ("missing arguments - expected 2 arguments: input_fragments output_sequence");
+
+    auto fragments = load_fragments (args[1]);
+    fragment_statistics (fragments);
+
+    unsigned int size_of_longest = 0;
+    unsigned int index_of_longest = 0;
+    for (unsigned int n = 0; n < fragments.size(); ++n) {
+      if (fragments[n].size() > size_of_longest) {
+        index_of_longest = n;
+        size_of_longest = fragments[n].size();
+      }
+    }
+
+    std::string sequence = fragments[index_of_longest];
+    std::cerr << "initial sequence has size " << sequence.size() << "\n";
+
+    write_sequence (args[2], sequence);
+
+  }
+  catch (std::exception& excp) {
+    std::cerr << "ERROR: " << excp.what() << " - aborting\n";
     return 1;
   }
-
-  auto fragments = load_fragments (args[1]);
-  fragment_statistics (fragments);
-
-
-  unsigned int size_of_longest = 0;
-  unsigned int index_of_longest = 0;
-  for (unsigned int n = 0; n < fragments.size(); ++n) {
-    if (fragments[n].size() > size_of_longest) {
-      index_of_longest = n;
-      size_of_longest = fragments[n].size();
-    }
+  catch (...) {
+    std::cerr << "ERROR: unknown exception thrown - aborting\n";
+    return 1;
   }
-  std::string sequence = fragments[index_of_longest];
-  std::cerr << "initial sequence has size " << sequence.size() << "\n";
-
-  write_sequence (args[2], sequence);
 
   return 0;
 }

jdtournier/cpp_course

projects/DNA_shotgun_sequencing/solution/shotgun.cpp
@@ -9,20 +9,16 @@ std::vector<std::string> load_fragments (const std::string& filename)
   std::cerr << "reading fragments from file \"" << filename << "\"...\n";
 
   std::ifstream infile (filename);
-  if (!infile) {
-    std::cerr << "ERROR: failed to open file \"" << filename << "\" - aborting\n";
-    std::exit (1);
-  }
+  if (!infile)
+    throw std::runtime_error ("failed to open file \"" + filename + "\"");
 
   std::vector<std::string> fragments;
   std::string frag;
   while (infile >> frag)
     fragments.push_back (frag);
 
-  if (fragments.empty()) {
-    std::cerr << "ERROR: file \"" << filename << "\" contains no fragments - aborting\n";
-    std::exit (1);
-  }
+  if (fragments.empty())
+    throw std::runtime_error ("file \"" + filename + "\" contains no fragments");
 
   std::cerr << "read " << fragments.size() << " fragments\n";
 
@@ -52,15 +48,13 @@ void write_sequence (const std::string& filename, const std::string& sequence)
 {
   std::cerr << "writing sequence to file \"" << filename << "\"...\n";
   std::ofstream outfile (filename);
-  if (!outfile) {
-    std::cerr << "ERROR: failed to open output file \"" << filename << "\" - aborting\n";
-    std::exit (1);
-  }
+  if (!outfile)
+    throw std::runtime_error ("failed to open output file \"" + filename + "\"");
+
   outfile << sequence << "\n";
-  if (!outfile) {
-    std::cerr << "ERROR: error occurred while writing to output file \"" << filename << "\" - aborting\n";
-    std::exit (1);
-  }
+
+  if (!outfile)
+    throw std::runtime_error ("error occurred while writing to output file \"" + filename + "\"");
 }
 
 
@@ -74,22 +68,35 @@ int main (int argc, char* argv[])
     return 1;
   }
 
-  auto fragments = load_fragments (argv[1]);
-  fragment_statistics (fragments);
+  try { // Start of main processing:
 
+    auto fragments = load_fragments (argv[1]);
+    fragment_statistics (fragments);
 
-  unsigned int size_of_longest = 0;
-  unsigned int index_of_longest = 0;
-  for (unsigned int n = 0; n < fragments.size(); ++n) {
-    if (fragments[n].size() > size_of_longest) {
-      index_of_longest = n;
-      size_of_longest = fragments[n].size();
+    unsigned int size_of_longest = 0;
+    unsigned int index_of_longest = 0;
+    for (unsigned int n = 0; n < fragments.size(); ++n) {
+      if (fragments[n].size() > size_of_longest) {
+        index_of_longest = n;
+        size_of_longest = fragments[n].size();
+      }
     }
-  }
-  std::string sequence = fragments[index_of_longest];
-  std::cerr << "initial sequence has size " << sequence.size() << "\n";
+    std::string sequence = fragments[index_of_longest];
+    std::cerr << "initial sequence has size " << sequence.size() << "\n";
 
-  write_sequence (argv[2], sequence);
+    write_sequence (argv[2], sequence);
+
+  } // end of main processing
+
+  // error handling from here:
+  catch (std::exception& excp) {
+    std::cerr << "ERROR: " << excp.what() << " - aborting\n";
+    return 1;
+  }
+  catch (...) {
+    std::cerr << "ERROR: unknown exception thrown - aborting\n";
+    return 1;
+  }
 
   return 0;
 }