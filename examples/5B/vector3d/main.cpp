bd2a5986419b4652cb1e881fbedb54ac51d452d8
J-Donald Tournier <jdournier@gmail.com>
2026-01-15 22:00:40 +0000

    Add equality operators to test for equality with a scalar value

--- examples/5B/vector3d/main.cpp
+++ examples/5B/vector3d/main.cpp
@@ -11,5 +11,9 @@ int main ()
   std::cout << "!v: " << !v << "\n";
   std::cout << "v==v2: " << (v==v2) << "\n";
 
+  v[1] = v[2] = 1.0;
+  std::cout << "v==1: " << (v==1.0) << "\n";
+  std::cout << "v2==1: " << (v2==1.0) << "\n";
+
   return 0;
 }
