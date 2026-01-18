bc6b91031b591bc0cc1937272520cd8a4fd26802
J-Donald Tournier <jdournier@gmail.com>
2026-01-18 20:12:56 +0000

    add equality and inequality operators to Image class

--- examples/5B/image/main.cpp
+++ examples/5B/image/main.cpp
@@ -10,5 +10,18 @@ int main ()
   Image b (10,10);
   std::cout << "Image b: " << b << "\n";
 
+  std::cout << "a == b: " << (a==b) << "\n";
+  std::cout << "a != b: " << (a!=b) << "\n";
+
+  auto c = b;
+
+  std::cout << "b == c: " << (b==c) << "\n";
+  std::cout << "b != c: " << (b!=c) << "\n";
+
+  c(1,1) = 1;
+
+  std::cout << "b == c: " << (b==c) << "\n";
+  std::cout << "b != c: " << (b!=c) << "\n";
+
   return 0;
 }
