90ffe86b822cc56d85072799402e9d3ff32b2168
J-Donald Tournier <jdournier@gmail.com>
2026-01-18 20:12:56 +0000

    add equality and inequality operators to Image class

--- examples/5B/image/image.h
+++ examples/5B/image/image.h
@@ -10,6 +10,16 @@ class Image {
     Image (int w, int h) : m_data (w*h,0), m_width (w), m_height (h) { }
     Image& operator= (const Image&) = default;
 
+    bool operator== (const Image& other) const {
+      if (m_width != other.m_width || m_height != other.m_height)
+        return false;
+      for (int j = 0; j < height(); j++)
+        for (int i = 0; i < width(); i++)
+          if ((*this)(i,j) != other(i,j))
+            return false;
+      return true;
+    }
+
     int width () const { return m_width; }
     int height () const { return m_height; }
 
