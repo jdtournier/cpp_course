3071f07cd27a922bb4667d41246cee3cfff1d800
J-Donald Tournier <jdournier@gmail.com>
2026-01-18 20:05:59 +0000

    add stream insertion operator to Image class

--- examples/5B/image/main.cpp
+++ examples/5B/image/main.cpp
@@ -5,10 +5,10 @@
 int main ()
 {
   Image a;
-  std::cout << "Image a: " << a.width() << " x " << a.height() << "\n";
+  std::cout << "Image a: " << a << "\n";
 
   Image b (10,10);
-  std::cout << "Image b: " << b.width() << " x " << b.height() << "\n";
+  std::cout << "Image b: " << b << "\n";
 
   return 0;
 }
