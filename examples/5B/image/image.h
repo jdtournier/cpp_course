1eda9c3af42bcb08187fe0d82a265c2333bcee8e
J-Donald Tournier <jdournier@gmail.com>
2026-01-18 20:03:10 +0000

    Create Image class

---
+++ examples/5B/image/image.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <vector>
+
+class Image {
+  public:
+    Image() = default;
+    Image(const Image&) = default;
+    Image (int w, int h) : m_data (w*h,0), m_width (w), m_height (h) { }
+    Image& operator= (const Image&) = default;
+
+    int width () const { return m_width; }
+    int height () const { return m_height; }
+
+    int& operator() (int i, int j) { return m_data[i+m_width*j]; }
+    const int& operator() (int i, int j) const { return m_data[i+m_width*j]; }
+
+  private:
+    std::vector<int> m_data;
+    int m_width = 0, m_height = 0;
+};
