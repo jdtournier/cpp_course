e77de92846a22c59acf1b0a0758e9efd098f06aa
J-Donald Tournier <jdournier@gmail.com>
2026-01-19 13:45:30 +0000

    add Image to Image basic arithmetic operators to Image class

--- examples/5B/image/main.cpp
+++ examples/5B/image/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <format>
 
 #include "image.h"
+
+// function to display all the values in the image - for testing:
+void print (const Image& a, const std::string& label = {})
+{
+  std::cout << std::format ("{:-<80s}\n", ""); // print a line: 80 '-'
+  if (label.size()) {
+    std::cout << "  " << label << ":\n";
+    std::cout << std::format ("{:-<80s}\n", ""); // print a line: 80 '-'
+  }
+  for (int j = 0; j < a.height(); j++) {
+    for (int i = 0; i < a.width(); i++)
+      std::cout << std::format ("{:>3} ", a(i,j));
+    std::cout << "\n";
+  }
+  std::cout << std::format ("{:-<80s}\n", ""); // print a line: 80 '-'
+}
+
 
 int main ()
 {
@@ -36,6 +54,18 @@ int main ()
   std::cout << "1>c: " << (1>c) << "\n";
   std::cout << "1>=c: " << (1>=c) << "\n";
 
+  a(2,2) = 2;
+  c(2,2) = -3;
+  c(2,1) = 3;
+  a(1,2) = 4;
+  a(1,1) = 5;
+  print (a, "a");
+  print (c, "c");
+
+  print (a+c, "a+c");
+  print (a-c, "a-c");
+  print (a*c, "a*c");
+  print (a/c, "a/c"); // <- crashes with divide by zero floating-point exception!
 
   return 0;
 }
