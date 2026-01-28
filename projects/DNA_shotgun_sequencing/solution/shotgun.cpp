KCL-BMEIS/OOP

Create the ShotgunSequencer class to represent the shotgun sequencing algorithm
In C++, *classes* is how we define new objects. A class extends the
concept of a struct by adding *methods* to imbue behaviour into the
object. Methods provide ways to interact with the object, without
exposing the data within the object directly.

Note that structs can have methods too, and are essentially equivalent
to classes, the main difference is that members are public by default in
structs, and private by default in classes. In practice, classes are
almost invariably preferred, unless the object is only destined to be
used as a lightweight container for a simple collection of data.

projects/DNA_shotgun_sequencing/solution/shotgun.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <algorithm>
-#include <format>
 
 #include "fragments.h"
 #include "overlap.h"
 #include "debug.h"
+#include "shotgun_sequencer.h"
 
 // This function contains our program's core functionality:
 
@@ -18,46 +17,19 @@ void run (std::vector<std::string>& args)
     throw std::runtime_error ("missing arguments - expected 2 arguments: input_fragments output_sequence");
 
   auto fragments = load_fragments (args[1]);
-  if (debug::verbose)
-    fragment_statistics (fragments);
 
-  auto sequence = extract_longest_fragment (fragments);
-  std::cerr << "initial sequence has size " << sequence.size() << "\n";
+  ShotgunSequencer solver;
+  solver.init (fragments);
 
-  while (fragments.size()) {
-    debug::log ("---------------------------------------------------");
-    debug::log (std::to_string (fragments.size()) + " fragments left");
+  std::cerr << "initial sequence has size " << solver.sequence().size() << "\n";
 
-    auto overlap = find_biggest_overlap (sequence, fragments);
+  while (solver.iterate());
 
-    if (overlap.fragment < 0)
-      break;
+  solver.check_remaining_fragments();
 
-    if (std::abs (overlap.size) < 10)
-      break;
+  std::cerr << "final sequence has length " << solver.sequence().size() << "\n";
 
-    debug::log (std::format ("fragment with biggest overlap is at index {}, overlap = {}",
-         overlap.fragment, overlap.size));
-
-    merge (sequence, fragments[overlap.fragment], overlap.size);
-    fragments.erase (fragments.begin() + overlap.fragment);
-  }
-
-  debug::log (std::format ("{} fragments remaining unmatched - checking whether already contained in sequence...",
-        fragments.size()));
-
-  int num_unmatched = 0;
-  for (const auto& frag : fragments) {
-    if (sequence.find (frag) == std::string::npos)
-      ++num_unmatched;
-  }
-
-  if (num_unmatched)
-    std::cerr << "WARNING: " << num_unmatched << " fragments remain unmatched!\n";
-
-  std::cerr << "final sequence has length " << sequence.size() << "\n";
-
-  write_sequence (args[2], sequence);
+  write_sequence (args[2], solver.sequence());
 }
 
 

jdtournier/cpp_course

projects/DNA_shotgun_sequencing/solution/shotgun.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <string>
 #include <stdexcept>
-#include <format>
 
 #include "fragments.h"
 #include "overlap.h"
 #include "debug.h"
+#include "shotgun_sequencer.h"
 
 int main (int argc, char* argv[])
 {
@@ -19,44 +19,19 @@ int main (int argc, char* argv[])
   try { // Start of main processing:
 
     auto fragments = load_fragments (argv[1]);
-    if (debug::verbose)
-      fragment_statistics (fragments);
 
-    auto sequence = extract_longest_fragment (fragments);
-    std::cerr << "initial sequence has size " << sequence.size() << "\n";
+    ShotgunSequencer solver;
+    solver.init (fragments);
 
-    while (fragments.size()) {
-      debug::log ("---------------------------------------------------");
-      debug::log (std::to_string (fragments.size()) + " fragments left");
+    std::cerr << "initial sequence has size " << solver.sequence().size() << "\n";
 
-      auto overlap = find_biggest_overlap (sequence, fragments);
+    while (solver.iterate());
 
-      // if no fragments found that overlap, or if largest overlap found is
-      // less than 10, stop algorithm:
-      if (overlap.fragment < 0 || std::abs (overlap.size) < 10)
-        break;
+    solver.check_remaining_fragments();
 
-      debug::log (std::format ("fragment with biggest overlap is at index {}, overlap = {}",
-            overlap.fragment, overlap.size));
+    std::cerr << "final sequence has length " << solver.sequence().size() << "\n";
 
-      merge (sequence, fragments[overlap.fragment], overlap.size);
-      fragments.erase (fragments.begin() + overlap.fragment);
-    }
-
-    debug::log (std::format ("{} fragments remaining unmatched - checking whether already contained in sequence...",
-          fragments.size()));
-
-    int num_unmatched = 0;
-    for (const auto& frag : fragments) {
-      if (sequence.find (frag) == std::string::npos)
-        ++num_unmatched;
-    }
-
-    if (num_unmatched)
-      std::cerr << "WARNING: " << num_unmatched << " fragments remain unmatched!\n";
-
-    std::cerr << "final sequence has length " << sequence.size() << "\n";
-    write_sequence (argv[2], sequence);
+    write_sequence (argv[2], solver.sequence());
 
   } // end of main processing
 