KCL-BMEIS/OOP

Add function to read PGM file
We set up a function to load a PGM file, though for now we will only
read the information at the head of the file (the *image header*), and
report the size of the image, along with the maximum intensity value.

projects/fMRI/solution/fmri.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 
 #include "debug.h"
+#include "pgm.h"
 
 // This function contains our program's core functionality:
 
@@ -14,7 +15,7 @@ void run (std::vector<std::string>& args)
   if (args.size() < 2)
     throw std::runtime_error ("missing arguments - expected at least 1 argument");
 
-  std::cerr << "reading file \"" << args[1] << "\"\n";
+  auto slice = load_pgm (args[1]);
 
 }
 

jdtournier/cpp_course

projects/fMRI/solution/fmri.cpp
@@ -2,6 +2,7 @@
 #include <stdexcept>
 
 #include "debug.h"
+#include "pgm.h"
 
 int main (int argc, char* argv[])
 {
@@ -14,6 +15,7 @@ int main (int argc, char* argv[])
 
   try { // Start of main processing:
 
+    auto slice = load_pgm (argv[1]);
 
   } // end of main processing
 