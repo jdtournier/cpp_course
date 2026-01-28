KCL-BMEIS/OOP

Add function to compute image of correlation coefficients
We now have all the pieces required to complete the project, with a
function to loop over all the pixels in the input data, and compute the
correlation coefficient of each pixel's timecourse with the task.

Since our Image class can only store data of type int, we cannot store
the correlation coefficient values as they are, since they scale from -1
to 1. We therefore scale the results by 1000 to store them in the
resulting image of coefficients.

projects/fMRI/solution/fmri.cpp
@@ -60,6 +60,9 @@ void run (std::vector<std::string>& args)
 
   std::cerr << std::format ("correlation_coefficient at ({},{}) = {}\n",
      x, y, correlation_coefficient (signal, task));
+
+  auto im_corr = correlation_coefficient (task, data);
+  TG::imshow (TG::magnify (im_corr, 4), -1000, 1000, TG::jet());
 }
 
 

jdtournier/cpp_course

projects/fMRI/solution/fmri.cpp
@@ -89,6 +89,9 @@ int main (int argc, char* argv[])
     std::cerr << std::format ("correlation_coefficient at ({},{}) = {}\n",
         pixel.x, pixel.y, correlation_coefficient (signal, task));
 
+    auto im_corr = correlation_coefficient (task, data);
+    termviz::imshow (termviz::magnify (im_corr, 4), -1000, 1000, termviz::jet());
+
   } // end of main processing
 
   // error handling from here: