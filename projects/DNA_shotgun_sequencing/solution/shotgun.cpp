KCL-BMEIS/OOP

Create functions to handle loading and statistics
Distinct tasks can be handled by appropriately named functions, which
has multiple advantages:
- the function can if necessary be used in multiple places, avoiding
  needless duplication
- with well-chosen function names, the main code become much more
  readable

Note that it becomes harder to handle errors. We now need to call the
`std::exit()` function to guarantee program termination. We will see how
to deal with this better later on.

projects/DNA_shotgun_sequencing/solution/shotgun.cpp
@@ -3,20 +3,15 @@
 #include <string>
 #include <fstream>
 
-int main (int argc, char* argv[])
+
+std::vector<std::string> load_fragments (const std::string& filename)
 {
-  std::vector<std::string> args (argv, argv+argc);
+  std::cerr << "reading fragments from file \"" << filename << "\"...\n";
 
-  if (args.size() < 3) {
-    std::cerr << "ERROR: missing arguments - expected 2 arguments: input_fragments output_sequence\n";
-    return 1;
-  }
-  std::cerr << "reading fragments from file \"" << args[1] << "\"...\n";
-
-  std::ifstream infile (args[1]);
+  std::ifstream infile (filename);
   if (!infile) {
-    std::cerr << "ERROR: failed to open file \"" << args[1] << "\" - aborting\n";
-    return 1;
+    std::cerr << "ERROR: failed to open file \"" << filename << "\" - aborting\n";
+    std::exit (1);
   }
 
   std::vector<std::string> fragments;
@@ -25,12 +20,20 @@ int main (int argc, char* argv[])
     fragments.push_back (frag);
 
   if (fragments.empty()) {
-    std::cerr << "ERROR: file \"" << args[1] << "\" contains no fragments - aborting\n";
-    return 1;
+    std::cerr << "ERROR: file \"" << filename << "\" contains no fragments - aborting\n";
+    std::exit (1);
   }
 
   std::cerr << "read " << fragments.size() << " fragments\n";
 
+  return fragments;
+}
+
+
+
+
+void fragment_statistics (const std::vector<std::string>& fragments)
+{
   double sum = 0.0;
   std::string::size_type min = fragments[0].size();
   std::string::size_type max = fragments[0].size();
@@ -40,6 +43,41 @@ int main (int argc, char* argv[])
     max = std::max (max, f.size());
   }
   std::cerr << "mean fragment length: " << sum/fragments.size() << ", range [ " << min << " " << max << " ]\n";
+}
+
+
+
+
+void write_sequence (const std::string& filename, const std::string& sequence)
+{
+  std::cerr << "writing sequence to file \"" << filename << "\"...\n";
+  std::ofstream outfile (filename);
+  if (!outfile) {
+    std::cerr << "ERROR: failed to open output file \"" << filename << "\" - aborting\n";
+    std::exit (1);
+  }
+  outfile << sequence << "\n";
+  if (!outfile) {
+    std::cerr << "ERROR: error occurred while writing to output file \"" << filename << "\" - aborting\n";
+    std::exit (1);
+  }
+}
+
+
+
+
+
+int main (int argc, char* argv[])
+{
+  std::vector<std::string> args (argv, argv+argc);
+
+  if (args.size() < 3) {
+    std::cerr << "ERROR: missing arguments - expected 2 arguments: input_fragments output_sequence\n";
+    return 1;
+  }
+
+  auto fragments = load_fragments (args[1]);
+  fragment_statistics (fragments);
 
 
   unsigned int size_of_longest = 0;
@@ -53,18 +91,7 @@ int main (int argc, char* argv[])
   std::string sequence = fragments[index_of_longest];
   std::cerr << "initial sequence has size " << sequence.size() << "\n";
 
-
-  std::cerr << "writing sequence to file \"" << args[2] << "\"...\n";
-  std::ofstream outfile (args[2]);
-  if (!outfile) {
-    std::cerr << "ERROR: failed to open output file \"" << args[2] << "\" - aborting\n";
-    return 1;
-  }
-  outfile << sequence << "\n";
-  if (!outfile) {
-    std::cerr << "ERROR: error occurred while writing to output file \"" << args[2] << "\" - aborting\n";
-    return 1;
-  }
+  write_sequence (args[2], sequence);
 
   return 0;
 }

jdtournier/cpp_course

projects/DNA_shotgun_sequencing/solution/shotgun.cpp
@@ -3,18 +3,15 @@
 #include <string>
 #include <fstream>
 
-int main (int argc, char* argv[])
-{
-  if (argc < 3) {
-    std::cerr << "ERROR: missing arguments - expected 2 arguments: input_fragments output_sequence\n";
-    return 1;
-  }
-  std::cerr << "reading fragments from file \"" << argv[1] << "\"...\n";
 
-  std::ifstream infile (argv[1]);
+std::vector<std::string> load_fragments (const std::string& filename)
+{
+  std::cerr << "reading fragments from file \"" << filename << "\"...\n";
+
+  std::ifstream infile (filename);
   if (!infile) {
-    std::cerr << "ERROR: failed to open file \"" << argv[1] << "\" - aborting\n";
-    return 1;
+    std::cerr << "ERROR: failed to open file \"" << filename << "\" - aborting\n";
+    std::exit (1);
   }
 
   std::vector<std::string> fragments;
@@ -23,12 +20,20 @@ int main (int argc, char* argv[])
     fragments.push_back (frag);
 
   if (fragments.empty()) {
-    std::cerr << "ERROR: file \"" << argv[1] << "\" contains no fragments - aborting\n";
-    return 1;
+    std::cerr << "ERROR: file \"" << filename << "\" contains no fragments - aborting\n";
+    std::exit (1);
   }
 
   std::cerr << "read " << fragments.size() << " fragments\n";
 
+  return fragments;
+}
+
+
+
+
+void fragment_statistics (const std::vector<std::string>& fragments)
+{
   double sum = 0.0;
   auto min = fragments[0].size();
   auto max = fragments[0].size();
@@ -38,6 +43,39 @@ int main (int argc, char* argv[])
     max = std::max (max, f.size());
   }
   std::cerr << "mean fragment length: " << sum/fragments.size() << ", range [ " << min << " " << max << " ]\n";
+}
+
+
+
+
+void write_sequence (const std::string& filename, const std::string& sequence)
+{
+  std::cerr << "writing sequence to file \"" << filename << "\"...\n";
+  std::ofstream outfile (filename);
+  if (!outfile) {
+    std::cerr << "ERROR: failed to open output file \"" << filename << "\" - aborting\n";
+    std::exit (1);
+  }
+  outfile << sequence << "\n";
+  if (!outfile) {
+    std::cerr << "ERROR: error occurred while writing to output file \"" << filename << "\" - aborting\n";
+    std::exit (1);
+  }
+}
+
+
+
+
+
+int main (int argc, char* argv[])
+{
+  if (argc < 3) {
+    std::cerr << "ERROR: missing arguments - expected 2 arguments: input_fragments output_sequence\n";
+    return 1;
+  }
+
+  auto fragments = load_fragments (argv[1]);
+  fragment_statistics (fragments);
 
 
   unsigned int size_of_longest = 0;
@@ -51,18 +89,7 @@ int main (int argc, char* argv[])
   std::string sequence = fragments[index_of_longest];
   std::cerr << "initial sequence has size " << sequence.size() << "\n";
 
-
-  std::cerr << "writing sequence to file \"" << argv[2] << "\"...\n";
-  std::ofstream outfile (argv[2]);
-  if (!outfile) {
-    std::cerr << "ERROR: failed to open output file \"" << argv[2] << "\" - aborting\n";
-    return 1;
-  }
-  outfile << sequence << "\n";
-  if (!outfile) {
-    std::cerr << "ERROR: error occurred while writing to output file \"" << argv[2] << "\" - aborting\n";
-    return 1;
-  }
+  write_sequence (argv[2], sequence);
 
   return 0;
 }