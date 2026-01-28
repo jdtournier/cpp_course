KCL-BMEIS/OOP

Final touches to project
We finalise the project by removing functionality that is no longer
being used, and change the behaviour of the program slightly: by
default, the map of correlation coefficients is computed and displayed.
However, if the -p x y option is provided, the timecourse for that pixel
is shown along with the task, and only the correlation coefficient for
this timecourse is computed and displayed.

projects/fMRI/solution/fmri.cpp
@@ -41,15 +41,17 @@ void run (std::vector<std::string>& args)
 
   TG::imshow (TG::magnify (data[0], 4), 0, 4000);
 
-  // default values if x & y not set (<0):
   if (x < 0 || y < 0) {
-    x = data[0].width()/2;
-    y = data[0].height()/2;
+    // if no pixel is specified, compute map of correlation coefficient:
+    auto im_corr = correlation_coefficient (task, data);
+    TG::imshow (TG::magnify (im_corr, 4), -1000, 1000, TG::jet());
+
+    return;
   }
-  else {
-    if (x >= data[0].width() || y >= data[0].height())
-      throw std::runtime_error ("pixel position is out of bounds");
-  }
+
+  // otherwise, extract that pixel's timecourse and plot against task:
+  if (x >= data[0].width() || y >= data[0].height())
+    throw std::runtime_error ("pixel position is out of bounds");
 
   auto signal = data.get_timecourse (x,y);
   auto minval = std::ranges::min (signal);
@@ -59,10 +61,7 @@ void run (std::vector<std::string>& args)
     .add_line (rescale (task, minval, maxval), 3);
 
   std::cerr << std::format ("correlation_coefficient at ({},{}) = {}\n",
-     x, y, correlation_coefficient (signal, task));
-
-  auto im_corr = correlation_coefficient (task, data);
-  TG::imshow (TG::magnify (im_corr, 4), -1000, 1000, TG::jet());
+      x, y, correlation_coefficient (signal, task));
 }
 
 

jdtournier/cpp_course

projects/fMRI/solution/fmri.cpp
@@ -74,9 +74,15 @@ int main (int argc, char* argv[])
     termviz::imshow (termviz::magnify (data[0], 4), 0, 4000);
 
     // default values if x & y not set (<0):
-    if (pixel.x < 0 || pixel.y < 0)
-      pixel = { data[0].width()/2, data[0].height()/2 };
-    else if (pixel.x >= data[0].width() || pixel.y >= data[0].height())
+    if (pixel.x < 0 || pixel.y < 0) {
+      auto im_corr = correlation_coefficient (task, data);
+      termviz::imshow (termviz::magnify (im_corr, 4), -1000, 1000, termviz::jet());
+
+      return 0;
+    }
+
+    // otherwise, extract that pixel's timecourse and plot against task:
+    if (pixel.x >= data[0].width() || pixel.y >= data[0].height())
       throw std::runtime_error ("pixel position is out of bounds");
 
     auto signal = data.get_timecourse (pixel.x, pixel.y);
@@ -88,9 +94,6 @@ int main (int argc, char* argv[])
 
     std::cerr << std::format ("correlation_coefficient at ({},{}) = {}\n",
         pixel.x, pixel.y, correlation_coefficient (signal, task));
-
-    auto im_corr = correlation_coefficient (task, data);
-    termviz::imshow (termviz::magnify (im_corr, 4), -1000, 1000, termviz::jet());
 
   } // end of main processing
 