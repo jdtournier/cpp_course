f29e205cc417b47089ff713841d7eae4d484868f
J-Donald Tournier <jdtournier@gmail.com>
2026-02-09 22:40:38 +0000

    Add visa status attribute, and add as additional argument to constructor with default value 'home'

--- examples/4B/student_class/student.h
+++ examples/4B/student_class/student.h
@@ -8,14 +8,17 @@ struct ModuleMark {
 };
 
 
+enum class VisaStatus { Home, International, Visitor };
+
 class Student {
   public:
-    Student () : m_ID (0), m_GPA (0.0), m_dob ({ 0,0,0 }) { }
-    Student (const std::string& name, const std::array<int,3>& dob, int ID) :
-      m_ID (ID), m_name (name), m_GPA (0.0), m_dob (dob) { }
+    Student () : m_ID (0), m_GPA (0.0), m_dob ({ 0,0,0 }), m_visa (VisaStatus::Home) { }
+    Student (const std::string& name, const std::array<int,3>& dob, int ID, VisaStatus visa = VisaStatus::Home) :
+      m_ID (ID), m_name (name), m_GPA (0.0), m_dob (dob), m_visa (visa) { }
 
-    Student (const std::string& name, const std::array<int,3>& dob, int ID, const ModuleMark& module_mark) :
-      Student (name, dob, ID) {
+    Student (const std::string& name, const std::array<int,3>& dob, int ID,
+        const ModuleMark& module_mark, VisaStatus visa = VisaStatus::Home) :
+      Student (name, dob, ID, visa) {
         m_marks.push_back (module_mark);
       }
 
@@ -23,6 +26,7 @@ class Student {
     const std::string& get_name () const { return m_name; }
     float get_GPA () const { return m_GPA; }
     const std::array<int,3>& get_DOB () const { return m_dob; }
+    VisaStatus get_visa_status () const { return m_visa; }
 
     void add_module_mark (const std::string& module_description, float mark, float weighting);
 
@@ -31,6 +35,7 @@ class Student {
     std::string m_name;
     float m_GPA;
     std::array<int,3> m_dob;
+    VisaStatus m_visa;
     std::vector<ModuleMark> m_marks;
 };
 
--- examples/4B/student_class/student_records.cpp
+++ examples/4B/student_class/student_records.cpp
@@ -4,7 +4,7 @@
 
 int main ()
 {
-  Student s ("Jane Doe", { 3, 6, 2001 }, 2349423, { "matlab", 3.1, 1.0 });
+  Student s ("Jane Doe", { 3, 6, 2001 }, 2349423, { "matlab", 3.1, 1.0 }, VisaStatus::International);
 
   s.add_module_mark ("mathematics", 3.2, 1.0);
   s.add_module_mark ("physics", 3.5, 2.0);
@@ -13,7 +13,13 @@ int main ()
 
   std::cout << "student: \"" << s.get_name() << "\", DOB: "
       << s.get_DOB()[0] << "/" << s.get_DOB()[1] << "/" << s.get_DOB()[2]
-      << ", ID: " << s.get_ID() << ", GPA = " << s.get_GPA() << "\n";
+      << ", ID: " << s.get_ID() << ", GPA = " << s.get_GPA() << " (visa status: ";
+  switch (s.get_visa_status()) {
+    case VisaStatus::Home: std::cout << "home"; break;
+    case VisaStatus::International: std::cout << "international"; break;
+    case VisaStatus::Visitor: std::cout << "visitor"; break;
+  };
+  std::cout << ")\n";
 
   return 0;
 }
