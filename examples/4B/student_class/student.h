239f0b8a86c54a61434eb91b306ff22e9438ff2a
J-Donald Tournier <jdtournier@gmail.com>
2026-02-09 20:51:34 +0000

    add default constructor to student class

--- examples/4B/student_class/student.h
+++ examples/4B/student_class/student.h
@@ -10,6 +10,7 @@ struct ModuleMark {
 
 class Student {
   public:
+    Student () : m_ID (0), m_GPA (0.0), m_dob ({ 0,0,0 }) { }
     int get_ID () const { return m_ID; }
     const std::string& get_name () const { return m_name; }
     float get_GPA () const { return m_GPA; }
@@ -25,10 +26,10 @@ class Student {
     void add_module_mark (const std::string& module_description, float mark, float weighting);
 
   private:
-    int m_ID = 0;
+    int m_ID;
     std::string m_name;
-    float m_GPA = 0.0;
-    std::array<int,3> m_dob = { 0, 0, 0 };
+    float m_GPA;
+    std::array<int,3> m_dob;
     std::vector<ModuleMark> m_marks;
 };
 
