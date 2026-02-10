83e5d0ef2a0113847fff3df8f7ce5291b03b17a0
J-Donald Tournier <jdournier@gmail.com>
2026-02-10 21:32:19 +0000

    move constructor definitions to separate cpp file

--- examples/4B/student_class/student.cpp
+++ examples/4B/student_class/student.cpp
@@ -2,7 +2,21 @@
 
 #include "student.h"
 
-void Student::add_module_mark (const std::string& module_description, float mark, float weighting)
+Student::Student () :
+  m_ID (0), m_GPA (0.0), m_dob ({ 0,0,0 }), m_visa (VisaStatus::Home) { }
+
+Student::Student (const std::string& name, const std::array<int,3>& dob,
+                  int ID, VisaStatus visa) :
+  m_ID (ID), m_name (name), m_GPA (0.0), m_dob (dob), m_visa (visa) { }
+
+Student::Student (const std::string& name, const std::array<int,3>& dob,
+                  int ID, const ModuleMark& module_mark, VisaStatus visa) :
+  Student (name, dob, ID, visa) {
+    m_marks.push_back (module_mark);
+  }
+
+void Student::add_module_mark (const std::string& module_description,
+                               float mark, float weighting)
 {
   // add marks for this module to the list:
   m_marks.push_back ({ module_description, mark, weighting });
--- examples/4B/student_class/student.h
+++ examples/4B/student_class/student.h
@@ -12,15 +12,11 @@ enum class VisaStatus { Home, International, Visitor };
 
 class Student {
   public:
-    Student () : m_ID (0), m_GPA (0.0), m_dob ({ 0,0,0 }), m_visa (VisaStatus::Home) { }
-    Student (const std::string& name, const std::array<int,3>& dob, int ID, VisaStatus visa = VisaStatus::Home) :
-      m_ID (ID), m_name (name), m_GPA (0.0), m_dob (dob), m_visa (visa) { }
-
-    Student (const std::string& name, const std::array<int,3>& dob, int ID,
-        const ModuleMark& module_mark, VisaStatus visa = VisaStatus::Home) :
-      Student (name, dob, ID, visa) {
-        m_marks.push_back (module_mark);
-      }
+    Student ();
+    Student (const std::string& name, const std::array<int,3>& dob,
+             int ID, VisaStatus visa = VisaStatus::Home);
+    Student (const std::string& name, const std::array<int,3>& dob,
+             int ID, const ModuleMark& module_mark, VisaStatus visa = VisaStatus::Home);
 
     int get_ID () const { return m_ID; }
     const std::string& get_name () const { return m_name; }
