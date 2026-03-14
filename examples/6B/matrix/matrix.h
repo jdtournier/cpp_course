b094cd1f991eab48ac4fb11a5c59540a850ce1b3
J-Donald Tournier <jdournier@gmail.com>
2026-03-14 11:32:11 +0000

    Extend Matrix class to allow arbitrary, non-square size

--- examples/6B/matrix/matrix.h
+++ examples/6B/matrix/matrix.h
@@ -4,23 +4,23 @@
 #include <array>
 #include <format>
 
-// our matrix class to hold NxN matrices:
-template <typename X, int N> class Matrix {
+// our matrix class to hold MxN matrices:
+template <typename X, int M, int N> class Matrix {
   public:
     Matrix() = default;
 
-    int rows() const { return N; }
+    int rows() const { return M; }
     int cols() const { return N; }
 
     X& operator() (int i, int j) { return m_data[i][j]; }
     const X& operator() (int i, int j) const { return m_data[i][j]; }
 
   private:
-    std::array<std::array<X,N>,N> m_data;
+    std::array<std::array<X,N>,M> m_data;
 };
 
 // stream insertion operator for Matrix class:
-template <typename X, int N> std::ostream& operator<< (std::ostream& stream, const Matrix<X,N>& mat)
+template <typename X, int M, int N> std::ostream& operator<< (std::ostream& stream, const Matrix<X,M,N>& mat)
 {
   // Note this displays the matrix content over 3 lines.
   // There are other ways to display this if you wish!
