a44e68b2ac4ce90f4dd177125b03f99fd92fb9e5
J-Donald Tournier <jdournier@gmail.com>
2026-03-14 11:24:51 +0000

    Extend Matrix template class to allow arbitrary size

--- examples/6B/matrix/main.cpp
+++ examples/6B/matrix/main.cpp
@@ -4,13 +4,12 @@
 
 int main()
 {
-  Matrix<double> M;
+  Matrix<double,3> M;
   M(0,0) = 1.0; M(0,1) = 0.3; M(0,2) = 0.4;
   M(1,0) = 0.3; M(1,1) = 1.0; M(1,2) = 0.5;
   M(2,0) = 0.4; M(2,1) = 0.5; M(2,2) = 1.0;
 
   std::cout << "Matrix is " << M << "\n";
-  std::cout << "Determinant = " << compute_determinant (M) << "\n";
 
   return 0;
 }
--- examples/6B/matrix/matrix.h
+++ examples/6B/matrix/matrix.h
@@ -4,43 +4,32 @@
 #include <array>
 #include <format>
 
-// our matrix class to hold 3x3 matrices:
-template <typename X> class Matrix {
+// our matrix class to hold NxN matrices:
+template <typename X, int N> class Matrix {
   public:
     Matrix() = default;
 
-    int rows() const { return 3; }
-    int cols() const { return 3; }
+    int rows() const { return N; }
+    int cols() const { return N; }
 
     X& operator() (int i, int j) { return m_data[i][j]; }
     const X& operator() (int i, int j) const { return m_data[i][j]; }
 
   private:
-    std::array<std::array<X,3>,3> m_data;
+    std::array<std::array<X,N>,N> m_data;
 };
 
 // stream insertion operator for Matrix class:
-template <typename X> std::ostream& operator<< (std::ostream& stream, const Matrix<X>& M)
+template <typename X, int N> std::ostream& operator<< (std::ostream& stream, const Matrix<X,N>& mat)
 {
   // Note this displays the matrix content over 3 lines.
   // There are other ways to display this if you wish!
-  stream << std::format (
-      "3x3 matrix with contents:\n"
-      "    {:6.4f} {:6.4f} {:6.4f}\n"
-      "    {:6.4f} {:6.4f} {:6.4f}\n"
-      "    {:6.4f} {:6.4f} {:6.4f}\n",
-      M(0,0), M(0,1), M(0,2),
-      M(1,0), M(1,1), M(1,2),
-      M(2,0), M(2,1), M(2,2));
-
+  stream << std::format ("{}x{} matrix with contents:\n", mat.rows(), mat.cols());
+  for (int i = 0; i < mat.rows(); i++) {
+    for (int j = 0; j < mat.cols(); j++)
+      stream << std::format (" {:6.4f}", mat(i,j));
+    stream << "\n";
+  }
   return stream;
 }
 
-
-// function to compute determinant of 3x3 matrix:
-template <typename X> X compute_determinant (const Matrix<X>& M)
-{
-  return M(0,0) * ( M(1,1)*M(2,2) - M(2,1)*M(1,2) )
-       - M(1,0) * ( M(0,1)*M(2,2) - M(2,1)*M(0,2) )
-       + M(2,0) * ( M(0,1)*M(1,2) - M(1,1)*M(0,2) );
-}
