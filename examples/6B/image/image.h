e1b72e200dec859fc19e4db9753643ab075cc850
J-Donald Tournier <jdournier@gmail.com>
2026-03-14 09:57:20 +0000

    add initial code for examples in 6B

---
+++ examples/6B/image/image.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <vector>
+#include <ostream>
+
+class Image {
+  public:
+    Image() = default;
+    Image(const Image&) = default;
+    Image (int w, int h) : m_data (w*h,0), m_width (w), m_height (h) { }
+    Image& operator= (const Image&) = default;
+
+    bool operator== (const Image& other) const {
+      if (m_width != other.m_width || m_height != other.m_height)
+        return false;
+      for (int j = 0; j < height(); j++)
+        for (int i = 0; i < width(); i++)
+          if ((*this)(i,j) != other(i,j))
+            return false;
+      return true;
+    }
+
+    void resize (int w, int h) {
+      m_data.resize (w*h);
+      m_width = w;
+      m_height = h;
+    }
+
+    int width () const { return m_width; }
+    int height () const { return m_height; }
+
+    int& operator() (int i, int j) { return m_data[i+m_width*j]; }
+    const int& operator() (int i, int j) const { return m_data[i+m_width*j]; }
+
+  private:
+    std::vector<int> m_data;
+    int m_width = 0, m_height = 0;
+};
+
+
+
+inline std::ostream& operator<< (std::ostream& stream, const Image& im)
+{
+  stream << im.width() << " x " << im.height();
+  return stream;
+}
+
---
+++ examples/6B/image/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+#include "image.h"
+
+int main ()
+{
+  Image a;
+  std::cout << "Image a: " << a << "\n";
+
+  Image b (10,10);
+  std::cout << "Image b: " << b << "\n";
+
+  std::cout << "a == b: " << (a==b) << "\n";
+  std::cout << "a != b: " << (a!=b) << "\n";
+
+  auto c = b;
+
+  std::cout << "b == c: " << (b==c) << "\n";
+  std::cout << "b != c: " << (b!=c) << "\n";
+
+  c(1,1) = 1;
+
+  std::cout << "b == c: " << (b==c) << "\n";
+  std::cout << "b != c: " << (b!=c) << "\n";
+
+  a.resize (10,10);
+  std::cout << "Image a: " << a << "\n";
+
+
+  return 0;
+}
